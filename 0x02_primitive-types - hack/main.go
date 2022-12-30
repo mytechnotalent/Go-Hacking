f99dca1ae136ae1feb8f4a510652fe88ce94113b
Kevin Thomas <kevin@mytechnotalent.com>
2022-12-30 14:08:27 -0500

    Added files for Chapter 6

---
+++ 0x02_primitive-types - hack/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	b := true
+	i := 42
+	f := 3.14
+	s := "42"
+
+	fmt.Println("bool: ", b)
+	fmt.Println("int: ", i)
+	fmt.Println("float32: ", f)
+	fmt.Println("string: ", s)
+}
