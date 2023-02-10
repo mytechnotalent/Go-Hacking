7c1b38833ea8f0d4b1d99d45ff3012ddafc65dda
Kevin Thomas <kevin@mytechnotalent.com>
2023-02-10 12:03:47 -0500

    Added files for Chapter 7

---
+++ 0x03_control-flow/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	num := 42
+
+	if num == 42 {
+		fmt.Println(num, "the answer to life")
+	} else {
+		fmt.Println(num, "not the answer to life")
+	}
+}
