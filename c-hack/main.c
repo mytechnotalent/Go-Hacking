3113265a6d8f64af060f62c58d7e34183428e881
Kevin Thomas <kevin@mytechnotalent.com>
2023-04-13 08:44:52 -0700

    Update

--- c-hack/main.c
+++ c-hack/main.c
@@ -2,12 +2,16 @@
 #include <time.h>
 #include <windows.h>
 
-void win() {
-    printf("code flow successfully changed\n");
-}
+void win();
+int add(int a, int b, int c, int d, int e, int f, int g, int h);
 
 int main() {
+    int sum = 0;
+
     printf("normal flow\n");
+
+    sum = add(1, 2, 3,4, 5, 6, 7, 8);
+    printf("sum: %d\n", sum);
 
     time_t t1, t2;
     time(&t1);
@@ -19,4 +23,12 @@ int main() {
     }
 
     return 0;
+}
+
+void win() {
+    printf("code flow successfully changed\n");
+}
+
+int add(int a, int b, int c, int d, int e, int f, int g, int h) {
+    return a + b + c + d + e + f + g + h;
 }
